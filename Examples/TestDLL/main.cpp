194b27562f0ede855d8b069f3fdc7e151d8b0e41
Johann <goldnotch@mail.ru>
2023-10-15 10:01:45 +0500

    log output in cmake add test_dll example

--- Examples/TestDLL/main.cpp
+++ Examples/TestDLL/main.cpp
@@ -1,37 +1,23 @@
 // Copyright 2023 JohnCorn
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include <cstdio>
-#include <thread>
-#include <cmath>
-//#include <Framework/Window.hpp>
-
-//using namespace Framework;
-// ---------------------------- client app ----------------------------
-
-
-// ---------------------------- main -------------------------------
+#include <api.h>
+#include <iostream>
 
 int main()
 {
-	// Renderer renderer;
-	//OSWindow& main_wnd = OSWindow::CreateMainWindow(500, 600, "main_window");
-	
-	//OSWindow::RunWindows();
-	// init window and renderer
-	// ...
-
-
-	
-	return 0;
+    DllClass obj;
+    obj.print_foo();
+    std::cin.get();
+    return 0;
 }
