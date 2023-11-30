b3b466c86a15a11d2e52defb81f04692f8bd4b4d
John <goldnotch@mail.ru>
2023-11-30 21:06:55 +0500

    compilable project

---
+++ Source/EngineLib/Engine.h
@@ -0,0 +1,20 @@
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct Engine;
+typedef EnginePtr struct Engine*;
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif // ENGINE_H
