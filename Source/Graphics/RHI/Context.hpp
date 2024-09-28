28a4ceda2eed0228ed96f9907f242abd3f0cd465
John <goldnotch@mail.ru>
2024-09-28 20:40:51 +0200

    remove deleted folder added comments

--- Source/Graphics/RHI/Context.hpp
+++ Source/Graphics/RHI/Context.hpp
@@ -6,19 +6,24 @@
 namespace RHI
 {
 
+/// @brief Operation System handle
 using ExternalHandle = void *;
+/// @brief Internal handle (may be vulkan/DirectX/Metal object)
+using InternalObjectHandle = void *;
 
+/// @brief Generic settings to create a surface
 struct SurfaceConfig
 {
+  /// Generic window Handle (HWND Handle in Windows, X11Window handle in Linux)
   ExternalHandle hWnd;
+  /// Additional window handle (HINSTANCE handle in Windows, Display in Linux)
   ExternalHandle hInstance;
 };
 
-using InternalObjectHandle = void *;
 
 #define BIT(x) (1 << (x))
-
-enum class ShaderType
+/// @brief types of shader which can be attached to pipeline
+enum class ShaderType : uint8_t
 {
   Vertex = BIT(0),
   TessellationControl = BIT(1),
@@ -28,38 +33,42 @@ enum class ShaderType
   Compute = BIT(5),
 };
 
-enum class MeshTopology
+/// @brief a way to connect and interpret vertices in VertexData
+enum class MeshTopology : uint8_t
 {
-  Point,
-  Line,
-  LineStrip,
-  Triangle,
-  TriangleFan,
-  TriangleStrip
+  Point,        ///< every vertex is point
+  Line,         ///< vertices grouped by 2 and draws as line
+  LineStrip,    ///< vertices grouped by 2 and draws as line
+  Triangle,     ///< vertices grouped by 3 and draws as triangle
+  TriangleFan,  ///< vertices grouped by 3 and draws as triangle
+  TriangleStrip ///< vertices grouped by 3 and draws as triangle
 };
 
-enum class PolygonMode
+enum class PolygonMode : uint8_t
 {
-  Fill,
-  Line,
-  Point
+  Fill, ///< Fill polygon by color
+  Line, ///< Draw only border of polygon
+  Point ///< Draw polygon as point
 };
 
-enum class FrontFace
+/// @brief A way to define which polygon is front and which is back.
+enum class FrontFace : uint8_t
 {
   CW, //clockwise
   CCW // counter clockwise
 };
 
-enum class CullingMode
+/// @brief modes for culling
+enum class CullingMode : uint8_t
 {
-  None,
-  FrontFace,
-  BackFace,
-  FrontAndBack
+  None,        ///< Culling mode is disabled
+  FrontFace,   ///< front faces will be culled
+  BackFace,    ///< back faces will be culled
+  FrontAndBack ///< front and back polygons will be culled
 };
 
-enum class BlendOperation
+/// @brief binary operation used in blending. It's like operation(source_factor * source_color, dest_factor * dest_color)
+enum class BlendOperation : uint8_t
 {
   Add,              // src.color + dst.color
   Subtract,         // src.color - dst.color
@@ -68,9 +77,10 @@ enum class BlendOperation
   Max               // max(src.color, dst.color)
 };
 
-enum class BlendFactor
+/// @brief factor, which will be multiplied on color in blending
+enum class BlendFactor : uint8_t
 {
-  Zero,
+  Zero, ///<
   One,
   SrcColor,
   OneMinusSrcColor,
@@ -91,7 +101,8 @@ enum class BlendFactor
   OneMinusSrc1Alpha,
 };
 
-enum class ShaderImageSlot
+/// @brief
+enum class ShaderImageSlot : uint8_t
 {
   Color,
   DepthStencil,
@@ -99,13 +110,15 @@ enum class ShaderImageSlot
   TOTAL
 };
 
-enum class CommandBufferType
+/// @brief types of command buffers
+enum class CommandBufferType : uint8_t
 {
   Executable, ///< executable in GPU
   ThreadLocal ///< filled in separate thread
 };
 
-enum class BufferGPUUsage
+/// @brief enum to define usage of BufferGPU
+enum class BufferGPUUsage : uint8_t
 {
   VertexBuffer,
   IndexBuffer,
@@ -115,36 +128,37 @@ enum class BufferGPUUsage
   IndirectBuffer
 };
 
-enum class InputBindingType
+/// @brief defines what input attribute is used for. For instance or for each vertex
+enum class InputBindingType : uint8_t
 {
-  VertexData,
-  InstanceData
+  VertexData,  ///< input attribute will be associated with vertex
+  InstanceData ///< input attribute will be associeated with instance
 };
 
-enum class InputAttributeElementType
+/// @brief types for input attributes
+enum class InputAttributeElementType : uint8_t
 {
-  FLOAT,
-  DOUBLE,
-  UINT,
-  SINT
+  FLOAT,  ///< Input attribute will be interpreted in shader as float
+  DOUBLE, ///< Input attribute will be interpreted in shader as double
+  UINT,   ///< Input attribute will be interpreted in shader as unsigned int
+  SINT    ///< Input attribute will be interpreted in shader as signed int
 };
 
-enum class IndexType
+/// @brief data type for vertex indices
+enum class IndexType : uint8_t
 {
-  UINT8,
-  UINT16,
-  UINT32
+  UINT8,  ///< indices will be interpreted in driver as uint8_t*
+  UINT16, ///< indices will be interpreted in driver as uint16_t*
+  UINT32  ///< indices will be interpreted in driver as uint32_t*
 };
 
 struct ICommandBuffer;
 struct IBufferGPU;
 
-struct IUniform
-{
-  virtual ~IUniform() = default;
-  virtual void Upload(const void * data, size_t size, size_t offset = 0) = 0;
-};
-
+/// @brief Pipeline is container for rendering state settings (like shaders, input attributes, uniforms, etc).
+/// It has two modes: editing and drawing. In editing mode you can change any settings (attach shaders, uniforms, set viewport, etc).
+/// After editing you must call Invalidate(), it rebuilds internal objects and applyies new configuration.
+/// After invalidate you can bind it to CommandBuffer and draw.
 struct IPipeline
 {
   virtual ~IPipeline() = default;
@@ -164,7 +178,7 @@ struct IPipeline
   virtual uint32_t GetSubpass() const = 0;
 };
 
-
+/// @brief Framebuffer is a set of images to render
 struct IFramebuffer
 {
   virtual ~IFramebuffer() = default;
@@ -177,7 +191,9 @@ struct IFramebuffer
   virtual InternalObjectHandle GetHandle() const = 0;
 };
 
-/// @brief frames owner. API for rendering frames
+/// @brief Swapchain is object for rendering frames and present them onto surface (OS window)
+/// Swapchain contains final command buffer which will be submitted to GPU for drawing.
+/// You can fill it with another command buffers (which can be filled in parallel)
 struct ISwapchain
 {
   virtual ~ISwapchain() = default;
@@ -195,14 +211,15 @@ struct ISwapchain
   virtual std::unique_ptr<ICommandBuffer> CreateCommandBuffer() const = 0;
 };
 
-/// @brief buffer for GPU commands
+/// @brief buffer for GPU commands (like bindVertexBuffer or draw something)
 struct ICommandBuffer
 {
   virtual ~ICommandBuffer() = default;
 
-  /// @brief draw vertices command
+  /// @brief draw vertices command (analog glDrawArrays)
   virtual void DrawVertices(uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex = 0,
                             uint32_t firstInstance = 0) const = 0;
+  /// @brief draw vertices with indieces (analog glDrawElements)
   virtual void DrawIndexedVertices(uint32_t indexCount, uint32_t instanceCount,
                                    uint32_t firstIndex = 0, int32_t vertexOffset = 0,
                                    uint32_t firstInstance = 0) const = 0;
@@ -210,41 +227,52 @@ struct ICommandBuffer
   virtual void SetViewport(float width, float height) = 0;
   /// @brief Set scissor command
   virtual void SetScissor(int32_t x, int32_t y, uint32_t width, uint32_t height) = 0;
+  /// @brief binds buffer as input attribute data
   virtual void BindVertexBuffer(uint32_t binding, const IBufferGPU & buffer,
                                 uint32_t offset = 0) = 0;
+  /// @brief binds buffer as index buffer
   virtual void BindIndexBuffer(const IBufferGPU & buffer, IndexType type, uint32_t offset = 0) = 0;
 
-  /// @brief Clears buffer
+  /// @brief Clears all commands from buffer
   virtual void Reset() const = 0;
   /// @brief Enable writing mode (only for thread local buffers). Also binds framebuffer and pipeline
   virtual void BeginWriting(const IFramebuffer & framebuffer, const IPipeline & pipeline) = 0;
   /// @brief disables writing mode
   virtual void EndWriting() = 0;
-  /// @brief add commands
+  /// @brief take commands from another buffer
   virtual void AddCommands(const ICommandBuffer & buffer) const = 0;
   /// @brief get type of buffer
   virtual CommandBufferType GetType() const noexcept = 0;
 };
 
+/// @brief Generic data buffer in GPU. You can map it on CPU memory and change.
+/// After mapping changed data can be sent to GPU. Use Flush method to be sure that data is sent
 struct IBufferGPU
 {
   using UnmapFunc = std::function<void(void *)>;
   using ScopedPointer = std::unique_ptr<void, UnmapFunc>;
 
   virtual ~IBufferGPU() = default;
+  /// @brief Map buffer into CPU memory.  It will be unmapped in end of scope
   virtual ScopedPointer Map() = 0;
+  /// @brief Sends changed buffer after Map to GPU
   virtual void Flush() const noexcept = 0;
+  /// @brief checks if buffer is mapped into CPU memory
   virtual bool IsMapped() const noexcept = 0;
-  virtual InternalObjectHandle GetHandle() const noexcept = 0;
+  /// @brief Get size of buffer in bytes
   virtual size_t Size() const noexcept = 0;
+  /// @brief internal handle (for internal usage)
+  virtual InternalObjectHandle GetHandle() const noexcept = 0;
 };
 
 
+/// @brief Context is a main container for all objects above. It can creates some user-defined objects like buffers, framebuffers, etc
 struct IContext
 {
   virtual ~IContext() = default;
   virtual ISwapchain & GetSwapchain() & noexcept = 0;
   virtual const ISwapchain & GetSwapchain() const & noexcept = 0;
+  /// @brief wait for GPU is idle
   virtual void WaitForIdle() const = 0;
 
   /// @brief create offscreen framebuffer
@@ -253,10 +281,12 @@ struct IContext
   virtual std::unique_ptr<IPipeline> CreatePipeline(const IFramebuffer & framebuffer,
                                                     uint32_t subpassIndex) const = 0;
 
+  /// @brief creates BufferGPU
   virtual std::unique_ptr<IBufferGPU> AllocBuffer(size_t size, BufferGPUUsage usage,
                                                   bool mapped = false) const = 0;
 };
 
+/// @brief Factory-function to create context
 std::unique_ptr<IContext> CreateContext(const SurfaceConfig & config);
 
 } // namespace RHI
