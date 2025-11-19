806ea1c072c8f2eb35a40ce37c0969c9771e569c
goldnotch <goldnotch@mail.ru>
2025-11-19 08:14:00 +0500

    add base vector operators

--- Source/GameFramework/Game/Math.hpp
+++ Source/GameFramework/Game/Math.hpp
@@ -29,12 +29,29 @@ struct GAME_FRAMEWORK_API Mat4d { double m[16] = {  1.0, 0.0, 0.0, 0.0,
                                                     0.0, 1.0, 0.0, 0.0,
                                                     0.0, 0.0, 1.0, 0.0,
                                                     0.0, 0.0, 0.0, 1.0}; };
-// clang-format on
 
-inline Vec3f operator-(const Vec3f & v) noexcept
-{
-  return {-v.x, -v.y, -v.z};
-}
+inline Vec2f operator-(const Vec2f& v) { return { -v.x, -v.y }; }
+inline Vec3f operator-(const Vec3f& v) { return { -v.x, -v.y, -v.z }; }
+inline Vec4f operator-(const Vec4f& v) { return { -v.x, -v.y, -v.z, -v.w }; }
+inline Vec2d operator-(const Vec2d& v) { return { -v.x, -v.y }; }
+inline Vec3d operator-(const Vec3d& v) { return { -v.x, -v.y, -v.z }; }
+inline Vec4d operator-(const Vec4d& v) { return { -v.x, -v.y, -v.z, -v.w }; }
+
+inline Vec2f operator+(const Vec2f& v1, const Vec2f& v2) { return { v1.x + v2.x, v1.y + v2.y }; }
+inline Vec3f operator+(const Vec3f& v1, const Vec3f& v2) { return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z }; }
+inline Vec4f operator+(const Vec4f& v1, const Vec4f& v2) { return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w }; }
+inline Vec2d operator+(const Vec2d& v1, const Vec2d& v2) { return { v1.x + v2.x, v1.y + v2.y }; }
+inline Vec3d operator+(const Vec3d& v1, const Vec3d& v2) { return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z }; }
+inline Vec4d operator+(const Vec4d& v1, const Vec4d& v2) { return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w }; }
+
+inline Vec2f operator*(const Vec2f& v, float m) { return { v.x * m, v.y * m }; }
+inline Vec3f operator*(const Vec3f& v, float m) { return { v.x * m, v.y * m, v.z * m }; }
+inline Vec4f operator*(const Vec4f& v, float m) { return { v.x * m, v.y * m, v.z * m, v.w * m }; }
+inline Vec2d operator*(const Vec2d& v, double m) { return { v.x * m, v.y * m }; }
+inline Vec3d operator*(const Vec3d& v, double m) { return { v.x * m, v.y * m, v.z * m }; }
+inline Vec4d operator*(const Vec4d& v, double m) { return { v.x * m, v.y * m, v.z * m, v.w * m }; }
+
+// clang-format on
 
 } // namespace GameFramework
 
